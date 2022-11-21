80c88b5c9edcd464960369b53bb0669d851c060c
vovatrykoz <88552890+vovatrykoz@users.noreply.github.com>
2022-11-21 14:12:03 +0100

    Delete build directory

--- build/grid.d
+++
@@ -1,5 +0,0 @@
-grid.o: D:/DSWork/lessons_in_prog/nds_tick_tack_toe/source/grid.cpp \
- D:/DSWork/lessons_in_prog/nds_tick_tack_toe/include/grid.h \
- D:/DSWork/lessons_in_prog/nds_tick_tack_toe/include/cell.h
-D:/DSWork/lessons_in_prog/nds_tick_tack_toe/include/grid.h:
-D:/DSWork/lessons_in_prog/nds_tick_tack_toe/include/cell.h:
