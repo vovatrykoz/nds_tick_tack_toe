6ce514eed013e19bbe341700ae2dc8df1c63684c
vovatrykoz <88552890+vovatrykoz@users.noreply.github.com>
2022-11-21 13:49:03 +0100

    Delete build directory

--- build/cell.d
+++
@@ -1,3 +0,0 @@
-cell.o: D:/DSWork/lessons_in_prog/nds_tick_tack_toe/source/cell.cpp \
- D:/DSWork/lessons_in_prog/nds_tick_tack_toe/include/cell.h
-D:/DSWork/lessons_in_prog/nds_tick_tack_toe/include/cell.h:
