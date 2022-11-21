feb7a653ece0f9404471d332e65f3929ce27bfcf
vovatrykoz <88552890+vovatrykoz@users.noreply.github.com>
2022-11-21 14:14:31 +0100

    Comment removed

--- source/main.cpp
+++ source/main.cpp
@@ -91,48 +91,7 @@ int main(void) {
 		swiWaitForVBlank();
 	}
 
-/*
-	touchPosition touch;
 
-	PrintConsole *console = consoleDemoInit();  
-	PrintConsole right = *console;	
-	
-	consoleSetWindow(console, 15,1,12,16);
-	consoleSetWindow(&right, 1,1,12,16);
-
-	consoleSelect(console);
-	iprintf(border);
-	consoleSelect(&right);
-	iprintf(border);
-
-	consoleSetWindow(console, 2,2,10,14);
-	consoleSetWindow(&right,16,2,10,14);
-	
-	while(1) 
-	{
-		int keys;
-		
-		scanKeys();
-		
-		keys = keysHeld();
-
-		if(keys & KEY_START) break;
-		
-		if(keys & KEY_TOUCH)
-		{
-			touchRead(&touch);
-			
-			if(touch.px < 128)
-				consoleSelect(console);
-			else
-				consoleSelect(&right);
-				
-			iprintf("\nT: %i", touch.px);
-		}
-
-		swiWaitForVBlank();
-	}
-*/
 	return 0;
 }
 
